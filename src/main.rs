308fc5cc2974de1928f27dbf949c64882485a49d
SK <tehrealair@gmail.com>
2024-05-31 15:33:54 +1000

    feat(main): boilerplate code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+
+use dotenvy::dotenv;
+use ntex::web;
+
+#[web::post("/query")]
+async fn query(req_body: String) -> impl web::Responder {
+    web::HttpResponse::Ok().body(req_body)
+}
+
+#[ntex::main]
+async fn main() -> std::io::Result<()> {
+    dotenv().ok();
+
+    let port = env::var("PORT")
+        .expect("PORT must be set")
+        .parse::<u16>().expect("PORT must be a valid number");
+
+    web::HttpServer::new(|| {
+        web::App::new()
+            .service(query)
+    })
+        .bind(("0.0.0.0", port))?
+        .run()
+        .await
 }
